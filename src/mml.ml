f3d904668423a56056f2a199bca992ebc45809ac
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2021-08-02 23:04:00 -0700

    Create mml.ml

---
+++ src/mml.ml
@@ -0,0 +1,10 @@
+(*
+ * Copyright (c) MML, JackProgramsJP
+ *
+ * This source code is licensed under the MIT license,
+ * which is found in the root directory of this source tree.
+ *)
+ 
+let () =
+  print_endline "mml is not made yet"
+;;
